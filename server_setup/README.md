14d22298be2408703df045d2f5ac1fc5101271b8
azuki774s <azuki774s@gmail.com>
2024-06-06 00:56:07 +0900

    server setup

---
+++ server_setup/README.md
@@ -0,0 +1,20 @@
+### サーバセットアップに利用したコマンドを整理するディレクトリ
+
+環境: Ubuntu 22.04
+
+```
+root@azk-sandbox:/tmp/asterisk-20.8.1# uname -a
+Linux azk-sandbox 5.15.0-79-generic #86-Ubuntu SMP Mon Jul 10 16:07:21 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux
+```
+
+### /etc/asterisk/pjsip.conf
+- Ref: https://qiita.com/xecus/items/3f816e9fa0b31e9f3c2b
+- 値は改変しているが、ベースとして参考にさせていただいた。
+
+### /etc/asterisk/extensions.conf
+- Ref: https://qiita.com/xecus/items/3f816e9fa0b31e9f3c2b
+- 値は改変しているが、ベースとして参考にさせていただいた。
+
+
+### クライアント
+- MizuDroid で発着信 1call 確認
---
+++ server_setup/setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# required root
+
+# apt update -y;
+# apt upgrade -y;
+# reboot
+
+apt -y update
+add-apt-repository universe
+apt apt -y install git curl wget libnewt-dev libssl-dev libncurses5-dev subversion libsqlite3-dev build-essential libjansson-dev libxml2-dev uuid-dev
+
+cd ~/tmp
+wget http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-20-current.tar.gz
+
+tar xvf asterisk-20-current.tar.gz
+cd asterisk-20.8.1/ # 細かいバージョンが変わるかも
+contrib/scripts/get_mp3_source.sh
+sudo contrib/scripts/install_prereq install
+
+./configure
+make menuselect
+
+## 変なTUIが出るので、JA-WAVっぽいやつにチェックを付けておく ##
+
+make
+make install
+make progdocs
+make samples
+make config
+ldconfig
